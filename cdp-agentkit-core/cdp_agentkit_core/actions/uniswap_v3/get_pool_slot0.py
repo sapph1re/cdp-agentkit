0981462d3090ac2bebb5b18acc19eb37f8cddfeb
Rohan Agarwal <rohan.agarwal.1@coinbase.com>
2024-11-07 12:38:57 -0500

    Fix

--- cdp-agentkit-core/cdp_agentkit_core/actions/uniswap_v3/get_pool_slot0.py
+++ cdp-agentkit-core/cdp_agentkit_core/actions/uniswap_v3/get_pool_slot0.py
@@ -9,11 +9,11 @@ from cdp_agentkit_core.actions.uniswap_v3.constants import (
 )
 
 UNISWAP_V3_GET_POOL_SLOT0_PROMPT = """
-This tool will get the slot0 for Uniswap V3 pools. The slot0 contains the sqrtPriceX96, tick, observationIndex, observationCardinality, observationCardinalityNext, feeProtocol, and unlocked.
+This tool will get the slot0 for Uniswap V3 pools. It takes in a networkId and pool contract address. The slot0 contains the sqrtPriceX96, tick, observationIndex, observationCardinality, observationCardinalityNext, feeProtocol, and unlocked.
 The sqrtPriceX96 is the current price of the pool as a sqrt(token1/token0) Q64.96 value.
-The tick is the current tick of the pool, i.e. according to the last tick transition that was run. This value may not always be equal to SqrtTickMath getTickAtSqrtRatio(sqrtPriceX96) if the price is on a tick boundary.
-The observationIndex is the index of the last oracle observation. The observationCardinality is the current maximum number of observations stored.
-The observationCardinalityNext is the next maximum number of observations. The feeProtocol is the current protocol fee as a percentage of the swap fee taken on withdrawal.
+The tick is the current tick, based on the last transition that was run. This value may not always equal SqrtTickMath getTickAtSqrtRatio(sqrtPriceX96) if the price is on a tick boundary.
+The observationIndex is the index of the last oracle observation. The observationCardinality is the maximum number of observations stored.
+The observationCardinalityNext is the next maximum number of observations. The feeProtocol is the current fee as a percentage of the swap fee taken on withdrawal.
 The unlocked is whether the pool is currently locked to reentrancy. Supported networks are Base Sepolia, Base Mainnet, Ethereum Mainnet, Polygon Mainnet, and Arbitrum Mainnet.
 """
 
